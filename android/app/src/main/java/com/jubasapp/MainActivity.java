de9f385925100ffe641f66dc5fd9b86070b5ae84
marcelo-de-santana <marcelondesantana@outlook.com>
2023-08-07 22:29:05 -0300

    Dependencias do React Navigation

--- android/app/src/main/java/com/jubasapp/MainActivity.java
+++ android/app/src/main/java/com/jubasapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jubasapp;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -28,5 +29,14 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  /**
+   * React navigation dependency
+   */
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
