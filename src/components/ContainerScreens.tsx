458f52abd7d339f2294ec836f13026b335d52b11
marcelo-de-santana <marcelondesantana@outlook.com>
2023-09-04 07:54:48 -0300

    Adicao de StatusBar

--- src/components/ContainerScreens.tsx
+++ src/components/ContainerScreens.tsx
@@ -1,9 +1,18 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import {theme} from '@styles';
 
 interface ScreenProps {
   children: React.ReactNode;
 }
 export function Screen({children}: ScreenProps) {
-  return <SafeAreaView style={theme.container}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={theme.container}>
+      <StatusBarComponent />
+      {children}
+    </SafeAreaView>
+  );
+}
+
+function StatusBarComponent() {
+  return <StatusBar backgroundColor={'#F2F2F2'} barStyle={'dark-content'} />;
 }
